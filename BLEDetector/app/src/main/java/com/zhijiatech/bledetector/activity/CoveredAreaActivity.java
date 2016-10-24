12a975fae9139e7cc8e4f178d03ff8a6d2a5a484
e <wujiafeng@zhijiatech.com>
2016-10-24 11:44:52 +0800

    新初版提交

---
+++ BLEDetector/app/src/main/java/com/zhijiatech/bledetector/activity/CoveredAreaActivity.java
@@ -0,0 +1,14 @@
+package com.zhijiatech.bledetector.activity;
+
+import android.os.Bundle;
+
+import com.zhijiatech.bledetector.R;
+
+public class CoveredAreaActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covered_area);
+    }
+}
---
+++ BLEDetector/app/src/main/java/com/zhijiatech/bledetector/view/LineWave.java
@@ -0,0 +1,44 @@
+package com.zhijiatech.bledetector.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.zhijiatech.bledetector.util.ScreenUtils;
+
+
+/**
+ * Created by Jiafeng on 2016/10/14.
+ */
+public class LineWave extends View {
+    private static final int WAVE_PAINT_COLOR = 0xAA2B8FFC;
+    private Paint mWavePaint;
+    private int mTotalWidth, mTotalHeight;
+
+    private float mWaveHeight;
+
+    public void setWaveHeight(float waveHeight) {
+        mWaveHeight = waveHeight;
+    }
+
+    public LineWave(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mTotalHeight = (ScreenUtils.getScreenHeight(context)*9)/10;
+        mTotalWidth = ScreenUtils.getScreenWidth(context);
+        mWaveHeight = 0;
+        mWavePaint = new Paint();
+        mWavePaint.setAntiAlias(true);
+        mWavePaint.setStyle(Paint.Style.FILL);
+        mWavePaint.setColor(WAVE_PAINT_COLOR);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawRect(0,mTotalHeight-mWaveHeight,mTotalWidth,mTotalHeight,mWavePaint);
+
+        postInvalidateDelayed(30);
+    }
+}
